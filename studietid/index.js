025b58c37d44c78fc18b092534be32db3a3241a5
DumbAugust <carlaugust.sponberg@gmail.com>
2024-09-09 13:36:37 +0200

    Proper setup of express which has public folder as default path.

--- studietid/index.js
+++ studietid/index.js
@@ -1,10 +1,21 @@
-const sql = require('./scripts/sql.js');
+
+// Express variables
 const express = require("express");
 const app = express();
+const session = require('express-session');
+const path = require('path').join(__dirname, 'public');
+
+// Modules
+const sql = require('./scripts/sql.js');
+
+// Defualt path
+app.use(express.static(path));
+
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+    res.redirect(__dirname + '/index.html');
 });
+
 app.listen(3000, function () {
-    console.log("Server is running on localhost3000");
+    console.log('Server is running on http://localhost:3000');
 });
