87f71ae1b52b90ae5fb85d56aa64d2294e75ad47
DumbAugust <carlaugust.sponberg@gmail.com>
2024-09-13 10:47:14 +0200

    Updated /addUser, to return messages and changed sql.addUser to return the correct values.

--- studietid/modules/sql.js
+++ studietid/modules/sql.js
@@ -16,18 +16,9 @@ function addUser(req, isAdmin, idRole)
     let sql = db.prepare(`INSERT INTO user (firstName, lastName, idRole, isAdmin, email)
          VALUES (?, ?, ?, ?, ?)`);
          
-    const info = sql.run(req.firstName, req.lastName, idRole, isAdmin, req.email);
+    sql.run(req.firstName, req.lastName, idRole, isAdmin, req.email);
 
-    sql = db.prepare(
-        `SELECT user.id as userID, role.id as roleID, role.name as role 
-        FROM user
-        inner join role on user.idRole = role.id
-        WHERE user.id = ?`);
-
-    let rows = sql.all(info.lastInsertRowid);
-    console.log("Rowslength:" + rows.length);
-
-    return rows[0];
+    return 0;
 };
 
 function addActivity(userID, idSubject, idRoom)
