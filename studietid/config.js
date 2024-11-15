9c2a653be04f58e0f1a1d6a891e4a3da1b9874be
DumbAugust <carlaugust.sponberg@gmail.com>
2024-11-15 13:47:32 +0100

    Fixed a minor leak, and have to fix a infinite load on google page.

--- studietid/config.js
+++ studietid/config.js
@@ -1,5 +1,5 @@
-export const GOOGLE_CLIENT_ID = '89692555128-8c1qpscl51e5pe1alnepqiata7i92ps6.apps.googleusercontent.com';
-export const GOOGLE_CLIENT_SECRET = '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';
+export const GOOGLE_CLIENT_ID = '89692555128-vinplicgdbtamkp4gqssnm56b87b41h3.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_SECRET = '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';
 export const SERVER_ROOT_URI = 'http://localhost:3000';
 export const UI_ROOT_URI = 'http://localhost:3000';
 export const JWT_SECRET = 'shhhhhh';
