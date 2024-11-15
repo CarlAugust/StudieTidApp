4e62b527667707e251303a0f1a419d9031459306
DumbAugust <carlaugust.sponberg@gmail.com>
2024-11-15 13:33:56 +0100

    Removed user, have to implent a feature to save users.

--- studietid/index.js
+++ studietid/index.js
@@ -30,9 +30,8 @@ passport.use(new GoogleStrategy({
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    console.log("profile: ", profile);
+    console.log("cb: ", cb);
   }
 ));
 
