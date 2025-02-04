abe37708ae734b4c757bd2c3c1af508e07b0612c
DumbAugust <carlaugust.sponberg@gmail.com>
2025-02-04 13:23:41 +0100

    Remove config oopsies

--- studietid/config.js
+++
@@ -1,7 +0,0 @@
-export const GOOGLE_CLIENT_ID = '89692555128-vinplicgdbtamkp4gqssnm56b87b41h3.apps.googleusercontent.com';
-export const GOOGLE_CLIENT_SECRET = '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';
-export const SERVER_ROOT_URI = 'http://localhost:3000';
-export const UI_ROOT_URI = 'http://localhost:3000';
-export const JWT_SECRET = 'shhhhhh';
-export const COOKIE_NAME = 'auth_token';
- 
