9b884609d3ab96390b3433a0c6fc4249f9fbfc10
DumbAugust <carlaugust.sponberg@gmail.com>
2025-02-04 13:24:08 +0100

    Comments removed

--- studietid/modules/passport.js
+++ studietid/modules/passport.js
@@ -45,37 +45,4 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-// passport.use(new MicrosoftStrategy({
-//     // Standard OAuth2 options
-//     clientID: 'applicationidfrommicrosoft',
-//     clientSecret: 'applicationsecretfrommicrosoft',
-//     callbackURL: "http://localhost:3000/auth/microsoft/callback",
-//     scope: ['user.read'],
-
-//     // Microsoft specific options
-
-//     // [Optional] The tenant ID for the application. Defaults to 'common'. 
-//     // Used to construct the authorizationURL and tokenURL
-//     tenant: 'common',
-
-//     // [Optional] The authorization URL. Defaults to `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize`
-//     authorizationURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-
-//     // [Optional] The token URL. Defaults to `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`
-//     tokenURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-
-//     // [Optional] The Microsoft Graph API version (e.g., 'v1.0', 'beta'). Defaults to 'v1.0'.
-//     graphApiVersion: 'v1.0',
-
-//     // [Optional] If true, will push the User Principal Name into the `emails` array in the Passport.js profile. Defaults to false.
-//     addUPNAsEmail: false,
-    
-//     // [Optional] The Microsoft Graph API Entry Point, defaults to https://graph.microsoft.com. Configure this if you are using Azure China or other regional version.
-//     apiEntryPoint: 'https://graph.microsoft.com',
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     return checkIfUserIsValid(accessToken, refreshToken, profile, done);
-//   }
-// ));
-
 export default passport;
